4609aa481e798fe2bdd8775f9e06d9ec44344fd8
vit0r <vitornascimentoaraujo@gmail.com>
2021-09-26 17:40:30 -0300

    update 1

--- blueprints/modulo1.py
+++ blueprints/modulo1.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 from flask import Blueprint
 
-modulo1_blueprint = Blueprint(name='modulo1', import_name=__name__)
+modulo1_blueprint = Blueprint(name="modulo1", import_name=__name__)
 
 
-@modulo1_blueprint.route('/', methods=['GET'])
+@modulo1_blueprint.route("/", methods=["GET"])
 def index():
-    return 'modulo1'
+    return "modulo1"
--- blueprints/modulo2.py
+++ blueprints/modulo2.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
 from flask import Blueprint
 
-modulo2_blueprint = Blueprint(name='modulo2', import_name=__name__, url_prefix='/modulo2/')
+modulo2_blueprint = Blueprint(
+    name="modulo2", import_name=__name__, url_prefix="/modulo2/"
+)
 
 
-@modulo2_blueprint.route('add/<message>', methods=['GET'])
-@modulo2_blueprint.route('add/', defaults={'message': 'sem mensagem no add'},methods=['GET'])
-@modulo2_blueprint.route('/', defaults={'message': 'sem mensagem'}, methods=['GET'])
+@modulo2_blueprint.route("add/<message>", methods=["GET"])
+@modulo2_blueprint.route(
+    "add/", defaults={"message": "sem mensagem no add"}, methods=["GET"]
+)
+@modulo2_blueprint.route("/", defaults={"message": "sem mensagem"}, methods=["GET"])
 def add(message):
-    return 'modulo2 - {}'.format(message)
+    return "modulo2 - {}".format(message)
